67edfc90c92d8aca647f5768ad3d86b6c16187ef
Ibraheem Tuffaha <ibraheem@milkstraw.ai>
2024-09-03 04:23:36 +0000

    Add sample code with input file & test cases

---
+++ workspace/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    cout << s << " " << s << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
